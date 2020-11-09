02fc24b7a74c529127e8332931ee8fd1532486d7
Wincenzo3000 <74179520+Wincenzo3000@users.noreply.github.com>
2020-11-09 11:24:38 +0200

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa6e8932c4ef2001e31d319",
+				"5fa6e8932c4ef2001e31d31b"
+			]
+		},
+		"5fa6e8932c4ef2001e31d319": {
+			"id": "5fa6e8932c4ef2001e31d319",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa6e8932c4ef2001e31d31b": {
+			"id": "5fa6e8932c4ef2001e31d31b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
